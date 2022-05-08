760066253df0e5638d3954cb9ab3d8bc86049bf8
Mansi Sahu <mansisahu2332@gmail.com>
2022-05-08 22:58:11 +0530

    Recovered lost changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { createClient, Provider } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
 import React from 'react';
 import theme from '../theme';
+
+const client = createClient({
+  autoConnect: true,
+  connectors() {
+    return [new InjectedConnector()];
+  },
+});
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider client={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
