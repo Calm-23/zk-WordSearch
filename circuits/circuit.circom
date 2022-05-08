06ab20849f1deb0c9825d052ef6a867f06014bed
Mansi Sahu <mansisahu2332@gmail.com>
2022-05-08 22:17:07 +0530

    Circuit modified

--- circuits/circuit.circom
+++ circuits/circuit.circom
@@ -1,6 +1,7 @@
 pragma circom 2.0.0;
 
 include "./node_modules/circomlib/circuits/comparators.circom";
+include "./node_modules/circomlib-matrix/circuits/matSub.circom";
 
 template WordSearch() {
     /*  
@@ -9,42 +10,51 @@ template WordSearch() {
      */
     signal input ogGrid[4][4]; 
     /*  
+    Selected state of grid.
+    It consists of letters represented by their ASCII values.
+     */
+    signal input selectedGrid[4][4]; 
+    /*  
     The grid submitted by the solver. 
     The position of the word submitted is represented by 1 in the respective cells and rest by 0. 
     */
     signal input subGrid[4][4];
     /* 
+    Matrix of the selected search word, represented by it's ASCII values.
+    */
+    signal input selectedWord[4];
+    /* 
     Matrix of the search word, represented by it's ASCII values.
     */
     signal input word[4];
+    signal output out;
 
-    component positionAns[4][4]; //checking where value is 1 in subGrid
-    component matchAns[4][4]; //checking is ASCII values are equal
-
-    for (var i = 0; i < 4; i++) {
-        var len = 0;
-        for( var j = 0 ; j < 4; j++) {
-            /*
-            Checking which cells of the grid the player has selected.
-            Cells with value 1 means that the user has selected that letter.  */
-            positionAns[i][j] = IsEqual();
-            positionAns[i][j].in[0] <== subGrid[i][j];
-            positionAns[i][j].in[1] <== 1;
-
-            /* 
-            Checking if the word actually lies in the cells selected by the player.
-            This is done by comparing the ASCII values of the given Word matrix and the selected cells by player.
-            */
-            matchAns[i][j] = IsEqual();
-            matchAns[i][j].in[0] <== ogGrid[i][j];
-            matchAns[i][j].in[1] <== word[j];
-
-            positionAns[i][j].out === matchAns[i][j].out;
-            len++;
+    signal tempGrid[4][4];
+    component equalCheckGrid[4][4];
+    for(var i = 0 ; i < 4 ; i++) {
+        for(var j = 0 ; j < 4 ; j++) {
+            equalCheckGrid[i][j] = IsEqual();
+            equalCheckGrid[i][j].in[0] <== ogGrid[i][j];
+            equalCheckGrid[i][j].in[1] <== selectedGrid[i][j];
+            tempGrid[i][j] <== equalCheckGrid[i][j].out;
         }
-        //This represents that the player has identified the 4 letter word correctly in the grid.
-        len === 4;
     }
+
+    signal subtractedGrid[4][4];
+    for(var i = 0; i < 4; i++) {
+        for(var j = 0; j < 4; j++) {
+            subtractedGrid[i][j] <== tempGrid[i][j] - subGrid[i][j];
+            subtractedGrid[i][j] === 0;
+        }
+    }
+
+    signal tempWord[4];
+    for (var i = 0; i < 4; i++) {
+        tempWord[i] <== selectedWord[i] - word[i];
+        tempWord[i] === 0;
+    }
+
+    out <== 1;
 } 
 
-component main{public [subGrid]} = WordSearch();
+component main = WordSearch();
--- circuits/circuit_cpp/circuit.cpp
+++ circuits/circuit_cpp/circuit.cpp
@@ -13,17 +13,17 @@ Circom_TemplateFunction _functionTable[3] = {
 IsZero_0_run,
 IsEqual_1_run,
 WordSearch_2_run };
-uint get_main_input_signal_start() {return 1;}
+uint get_main_input_signal_start() {return 2;}
 
-uint get_main_input_signal_no() {return 36;}
+uint get_main_input_signal_no() {return 56;}
 
-uint get_total_signal_no() {return 229;}
+uint get_total_signal_no() {return 190;}
 
-uint get_number_of_components() {return 65;}
+uint get_number_of_components() {return 33;}
 
 uint get_size_of_input_hashmap() {return 256;}
 
-uint get_size_of_witness() {return 85;}
+uint get_size_of_witness() {return 70;}
 
 uint get_size_of_constants() {return 3;}
 
@@ -146,10 +146,10 @@ void WordSearch_2_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::stri
 ctx->componentMemory[coffset].templateId = 2;
 ctx->componentMemory[coffset].templateName = "WordSearch";
 ctx->componentMemory[coffset].signalStart = soffset;
-ctx->componentMemory[coffset].inputCounter = 36;
+ctx->componentMemory[coffset].inputCounter = 56;
 ctx->componentMemory[coffset].componentName = componentName;
 ctx->componentMemory[coffset].idFather = componentFather;
-ctx->componentMemory[coffset].subcomponents = new uint[32];
+ctx->componentMemory[coffset].subcomponents = new uint[16];
 }
 
 void WordSearch_2_run(uint ctx_index,Circom_CalcWit* ctx){
@@ -162,29 +162,16 @@ u64 myId = ctx_index;
 u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
 FrElement* circuitConstants = ctx->circuitConstants;
 std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
-FrElement expaux[4];
-FrElement lvar[3];
+FrElement expaux[3];
+FrElement lvar[2];
 uint sub_component_aux;
 {
 uint aux_create = 0;
-int aux_cmp_num = 32+ctx_index+1;
-uint csoffset = mySignalStart+132;
-uint aux_dimensions[2] = {4,4};
-for (uint i = 0; i < 16; i++) {
-std::string new_cmp_name = "positionAns"+ctx->generate_position_array(aux_dimensions, 2, i);
-mySubcomponents[aux_create+i] = aux_cmp_num;
-IsEqual_1_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
-csoffset += 6 ;
-aux_cmp_num += 2;
-}
-}
-{
-uint aux_create = 16;
 int aux_cmp_num = 0+ctx_index+1;
-uint csoffset = mySignalStart+36;
+uint csoffset = mySignalStart+93;
 uint aux_dimensions[2] = {4,4};
 for (uint i = 0; i < 16; i++) {
-std::string new_cmp_name = "matchAns"+ctx->generate_position_array(aux_dimensions, 2, i);
+std::string new_cmp_name = "equalCheckGrid"+ctx->generate_position_array(aux_dimensions, 2, i);
 mySubcomponents[aux_create+i] = aux_cmp_num;
 IsEqual_1_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
 csoffset += 6 ;
@@ -197,7 +184,7 @@ PFrElement aux_dest = &lvar[0];
 // end load src
 Fr_copy(aux_dest,&circuitConstants[0]);
 }
-Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 24
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 34
 while(Fr_isTrue(&expaux[0])){
 {
 PFrElement aux_dest = &lvar[1];
@@ -205,21 +192,15 @@ PFrElement aux_dest = &lvar[1];
 // end load src
 Fr_copy(aux_dest,&circuitConstants[0]);
 }
-{
-PFrElement aux_dest = &lvar[2];
-// load src
-// end load src
-Fr_copy(aux_dest,&circuitConstants[0]);
-}
-Fr_lt(&expaux[0],&lvar[2],&circuitConstants[2]); // line circom 26
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[2]); // line circom 35
 while(Fr_isTrue(&expaux[0])){
 {
-uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 0);
+uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 0);
 {
 PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 1];
 // load src
 // end load src
-Fr_copy(aux_dest,&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 0)]);
+Fr_copy(aux_dest,&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 1)]);
 }
 // run sub component if needed
 if(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter)){
@@ -228,12 +209,12 @@ IsEqual_1_run(mySubcomponents[cmp_index_ref],ctx);
 }
 }
 {
-uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 0);
+uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 0);
 {
 PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 2];
 // load src
 // end load src
-Fr_copy(aux_dest,&circuitConstants[1]);
+Fr_copy(aux_dest,&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 17)]);
 }
 // run sub component if needed
 if(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter)){
@@ -242,36 +223,11 @@ IsEqual_1_run(mySubcomponents[cmp_index_ref],ctx);
 }
 }
 {
-uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 16);
-{
-PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 1];
+PFrElement aux_dest = &signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 57)];
 // load src
 // end load src
-Fr_copy(aux_dest,&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 16)]);
+Fr_copy(aux_dest,&ctx->signalValues[ctx->componentMemory[mySubcomponents[(((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 0)]].signalStart + 0]);
 }
-// run sub component if needed
-if(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter)){
-IsEqual_1_run(mySubcomponents[cmp_index_ref],ctx);
-
-}
-}
-{
-uint cmp_index_ref = (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 16);
-{
-PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 2];
-// load src
-// end load src
-Fr_copy(aux_dest,&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[2])) + 32)]);
-}
-// run sub component if needed
-if(!(--ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter)){
-IsEqual_1_run(mySubcomponents[cmp_index_ref],ctx);
-
-}
-}
-Fr_eq(&expaux[0],&ctx->signalValues[ctx->componentMemory[mySubcomponents[(((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 0)]].signalStart + 0],&ctx->signalValues[ctx->componentMemory[mySubcomponents[(((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[2]))) + 16)]].signalStart + 0]); // line circom 34
-if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template " << myTemplateName << " line 34. " <<  "Followed trace: " << ctx->getTrace(myId) << std::endl;
-assert(Fr_isTrue(&expaux[0]));
 {
 PFrElement aux_dest = &lvar[1];
 // load src
@@ -279,26 +235,93 @@ Fr_add(&expaux[0],&lvar[1],&circuitConstants[1]); // line circom 35
 // end load src
 Fr_copy(aux_dest,&expaux[0]);
 }
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[2]); // line circom 35
+}
 {
-PFrElement aux_dest = &lvar[2];
+PFrElement aux_dest = &lvar[0];
 // load src
-Fr_add(&expaux[0],&lvar[2],&circuitConstants[1]); // line circom 26
+Fr_add(&expaux[0],&lvar[0],&circuitConstants[1]); // line circom 34
 // end load src
 Fr_copy(aux_dest,&expaux[0]);
 }
-Fr_lt(&expaux[0],&lvar[2],&circuitConstants[2]); // line circom 26
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 34
 }
-Fr_eq(&expaux[0],&circuitConstants[2],&circuitConstants[2]); // line circom 37
-if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template " << myTemplateName << " line 37. " <<  "Followed trace: " << ctx->getTrace(myId) << std::endl;
+{
+PFrElement aux_dest = &lvar[0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 44
+while(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[2]); // line circom 45
+while(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 73)];
+// load src
+Fr_sub(&expaux[0],&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 57)],&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 33)]); // line circom 46
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_eq(&expaux[0],&signalValues[mySignalStart + (((4 * Fr_toInt(&lvar[0])) + (1 * Fr_toInt(&lvar[1]))) + 73)],&circuitConstants[0]); // line circom 47
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template " << myTemplateName << " line 47. " <<  "Followed trace: " << ctx->getTrace(myId) << std::endl;
+assert(Fr_isTrue(&expaux[0]));
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+Fr_add(&expaux[0],&lvar[1],&circuitConstants[1]); // line circom 45
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[2]); // line circom 45
+}
+{
+PFrElement aux_dest = &lvar[0];
+// load src
+Fr_add(&expaux[0],&lvar[0],&circuitConstants[1]); // line circom 44
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 44
+}
+{
+PFrElement aux_dest = &lvar[0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 52
+while(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[0])) + 89)];
+// load src
+Fr_sub(&expaux[0],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[0])) + 49)],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[0])) + 53)]); // line circom 53
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_eq(&expaux[0],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[0])) + 89)],&circuitConstants[0]); // line circom 54
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template " << myTemplateName << " line 54. " <<  "Followed trace: " << ctx->getTrace(myId) << std::endl;
 assert(Fr_isTrue(&expaux[0]));
 {
 PFrElement aux_dest = &lvar[0];
 // load src
-Fr_add(&expaux[0],&lvar[0],&circuitConstants[1]); // line circom 24
+Fr_add(&expaux[0],&lvar[0],&circuitConstants[1]); // line circom 52
 // end load src
 Fr_copy(aux_dest,&expaux[0]);
 }
-Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 24
+Fr_lt(&expaux[0],&lvar[0],&circuitConstants[2]); // line circom 52
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
 }
 }
 
